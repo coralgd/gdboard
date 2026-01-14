e2ac707ba13d465003fd16f7f22087d921ed5f2d
coralgd <fufianz@gmail.com>
2026-01-14 19:08:19 +0300

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,87 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import {
+getAuth,
+signInWithEmailAndPassword,
+createUserWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+
+const firebaseConfig = {
+apiKey: "AIzaSyB7QbVxGHR7Qm8o8Wzl_wxsCpzMUdS5rws",
+authDomain: "gdboard-add4a.firebaseapp.com",
+projectId: "gdboard-add4a",
+storageBucket: "gdboard-add4a.firebasestorage.app",
+messagingSenderId: "883125028740",
+appId: "1:883125028740:web:c504de4139943b8606bfd1"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+
+window.loginOrRegister = async function () {
+const email = document.getElementById("email").value;
+const password = document.getElementById("password").value;
+const error = document.getElementById("error");
+
+
+try {
+await signInWithEmailAndPassword(auth, email, password);
+location.href = "nick.html";
+} catch (e) {
+if (e.code === "auth/user-not-found") {
+try {
+await createUserWithEmailAndPassword(auth, email, password);
+location.href = "nick.html";
+} catch (err) {
+error.textContent = err.message;
+}
+} else {
+error.textContent = e.message;
+}
+}
+};
+```js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import {
+getAuth,
+signInWithEmailAndPassword,
+createUserWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+
+const firebaseConfig = {
+apiKey: "ВСТАВЬ_СВОЙ_API_KEY",
+authDomain: "PROJECT.firebaseapp.com",
+projectId: "PROJECT",
+appId: "APP_ID"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+
+window.loginOrRegister = async function () {
+const email = document.getElementById("email").value;
+const password = document.getElementById("password").value;
+const error = document.getElementById("error");
+
+
+try {
+await signInWithEmailAndPassword(auth, email, password);
+location.href = "nick.html";
+} catch (e) {
+if (e.code === "auth/user-not-found") {
+try {
+await createUserWithEmailAndPassword(auth, email, password);
+location.href = "nick.html";
+} catch (err) {
+error.textContent = err.message;
+}
+} else {
+error.textContent = e.message;
+}
+}
+};
